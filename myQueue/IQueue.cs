cf9ff5aea4e48aa61f5ed1798c11f2c39131061a
gabri <gabrielemanuzzato@gmail.com>
2026-03-20 11:16:06 +0100

    Aggiungere i file di progetto.

---
+++ myQueue/IQueue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueManuzzato
+{
+    internal interface IQueue<T>
+    {
+        public bool IsEmpty();
+        public void Enqueue(T item);
+        public T Dequeue();
+        public T Peek();
+        public void Clear();
+    }
+}
---
+++ myQueue/Queue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueManuzzato
+{
+    internal class Queue<T> : IQueue<T>
+    {
+        private Node<T>? head;
+
+        public int Count;
+
+        public Queue()
+        {
+            head = null;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            Count = 0;
+        }
+
+        public T Dequeue()
+        {
+            if (head == null)
+                throw new Exception("lista non valorizzata");
+
+            T value = head.Data;
+
+            if (head.Next == null)
+                head = null;
+            else
+                head = head.Next;
+
+            Count--;
+
+            return value;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(item);
+                Count++;
+                return;
+            }
+
+            Node<T> node = head;
+
+            while (node.Next != null)
+                node = node.Next;
+
+            node.Next = new Node<T>(item);
+
+            Count++;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        public T Peek()
+        {
+            if (head == null)
+                throw new Exception("Lista non valorizzata");
+
+            return head.Data;
+        }
+    }
+}
